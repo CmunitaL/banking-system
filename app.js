2775689eff8cbf5a1ff43b0b4cbae99d1f524b02
Valentina Tardon <claudio.munita.l@gmail.com>
2026-03-22 19:53:35 -0300

    Initialize project structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Project Fidelis Bank running');
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Running server');
+
+});
